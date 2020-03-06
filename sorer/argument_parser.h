4a38941003268dcc33899de2639987f648c0946c
Danijj <danijj89@gmail.com>
2020-03-06 17:46:10 -0500

    added destructor for argument parser in sorer

--- sorer/argument_parser.h
+++ sorer/argument_parser.h
@@ -31,6 +31,11 @@ public:
 		this->val2_ = 0;
 	}
 
+	~ArgumentParser() override {
+		delete[] this->filename_;
+		delete[] this->command_;
+	}
+
 	/**
 	 * Parses the inputs to the program given the argc and argv.
 	 * @param argc the argc
