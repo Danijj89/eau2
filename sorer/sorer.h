ceba94a79f8914f8be6543e050e6a2492e52fc53
Danijj <danijj89@gmail.com>
2020-03-06 16:32:44 -0500

    added util files

---
+++ sorer/sorer.h
@@ -0,0 +1,8 @@
+//
+// Created by daniele zhan on 3/6/20.
+//
+
+#ifndef EAU2_SORER_H
+#define EAU2_SORER_H
+
+#endif //EAU2_SORER_H
---
+++ util/array.h
@@ -0,0 +1,66 @@
+//lang:CwC
+
+
+#pragma once
+
+
+#include <cstdio>
+#include <cassert>
+#include "object.h"
+
+
+/**
+ * A superclass for resizable arrays.
+ * @authors zhan.d@husky.neu.edu & yuan.cao@husky.neu.edu
+ */
+class Array : Object {
+public:
+	size_t size_; // num of elements
+	size_t capacity_; // capacity of array
+
+	/**
+	 * Default constructor.
+	 */
+	Array() {
+		this->size_ = 0;
+		this->capacity_ = 4;
+	}
+
+	/**
+	 * Returns the length of this array.
+	 * @return the length.
+	 */
+	virtual size_t len() {
+		return this->size_;
+	}
+
+	/**
+	 * Asserts that the given index is not out of bounds.
+	 * @param idx the index.
+	 */
+	virtual void assertIndexInBounds(size_t idx) {
+		assert(idx >= 0 && idx < this->size_);
+	}
+
+	/**
+     * Empties this array of all its values.
+     */
+	virtual void clear() {
+		this->size_ = 0;
+	}
+
+	/**
+	 * Resize the array if it has reached its full capacity.
+	 */
+	virtual void resizeIfFull() {
+		if (this->size_ >= this->capacity_) {
+			this->resize();
+		}
+	}
+
+	/**
+	 * Actual resize method.
+	 * Need to be overridden by subclasses that uses this feature.
+	 */
+	virtual void resize() {}
+};
