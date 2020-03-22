80617c9e29a8671197dc4761086036160c46e248
Danijj <danijj89@gmail.com>
2020-03-22 11:15:22 -0400

    changed kvstore to use key and value array

--- src/kvstore/KVStore.h
+++ src/kvstore/KVStore.h
@@ -4,20 +4,21 @@
 #include "key.h"
 #include "value.h"
 #include "../network/node.h"
+#include "key_array.h"
+#include "value_array.h"
 
 class KVStore : public Node {
 public:
 	int id_;
-	std::vector<Key> keys_;
-	std::vector<Value> values_;
+	KeyArray* keys_;
+	ValueArray* values_;
 
 	Value* get(Key* k) {
 		if (k->getNodeId() == this->id_) {
-			size_t i = this->keys_->indexAt(k);
+			size_t i = this->keys_->indexOf(k);
 			if (i == -1) return nullptr;
 			return this->values_->get(i);
 		} else {
-
 			return this->requestValue(k, fd);
 		}
 	}
@@ -32,12 +33,6 @@ public:
 		Serializer s = Serializer();
 		s.serialize_key(k);
 		write_message(fd, &m);
-		while (true) {
-			if (this->field != nullptr) {
-				break;
-			}
-			sleep(10);
-		}
 	}
 
 
