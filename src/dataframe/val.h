73df7be39871234bcb01c4612b47dfea3ec76606
danijj89 <danijj89@gmail.com>
2020-03-29 12:54:48 -0400

    cleaned val class and added unit tests

--- src/dataframe/val.h
+++ src/dataframe/val.h
@@ -10,7 +10,9 @@ class Double;
 class Str;
 
 /**
- * Superclass of a boxed primitive value.
+ * Superclass of a primitive value boxer.
+ * Used by the Row class to box primitives.
+ * They are created empty. Values can be set.
  * The types supported are:
  * - 'B': boolean
  * - 'I': integer
@@ -19,31 +21,54 @@ class Str;
  */
 class Val : public Object {
 public:
-	char type_;
+	char type_; // type of val
 
 	Val(char type) : type_(type) {}
-	
+
+	/**
+	 * Get the type of this val
+	 * @return
+	 */
 	char getType() {
 		return this->type_;
 	}
-	
+
+	/**
+	 * Return this val as a Bool
+	 * @return
+	 */
 	virtual Bool* asBool() {
 		return nullptr;
 	}
-	
+
+	/**
+	 * Return this val as an Int
+	 * @return
+	 */
 	virtual Int* asInt() {
 		return nullptr;
 	}
-	
+
+	/**
+	 * Return this val as a Double
+	 * @return
+	 */
 	virtual Double* asDouble() {
 		return nullptr;
 	}
-	
+
+	/**
+	 * Return this val as a Str
+	 * @return
+	 */
 	virtual Str* asStr() {
 		return nullptr;
 	}
 };
 
+/**
+ * Represents a class to box a boolean value
+ */
 class Bool : public Val {
 public:
 	bool val_;
@@ -63,6 +88,9 @@ public:
 	}
 };
 
+/**
+ * Represents a class to box a integer value
+ */
 class Int : public Val {
 public:
 	int val_;
@@ -82,6 +110,9 @@ public:
 	}
 };
 
+/**
+ * Represents a class to box a double value
+ */
 class Double : public Val {
 public:
 	double val_;
@@ -101,6 +132,9 @@ public:
 	}
 };
 
+/**
+ * Represents a class to box a String value
+ */
 class Str : public Val {
 public:
 	String* val_ = nullptr;
---
+++ tests/dataframe/val_tests.h
@@ -0,0 +1,50 @@
+#pragma once
+
+
+#include <gtest/gtest.h>
+#include "../../src/dataframe/val.h"
+#include "../../src/util/string.h"
+
+void getType() {
+	Val* values[4] {new Bool(), new Int(), new Double(), new Str()};
+	ASSERT_EQUALS(values[0]->getType(), 'B');
+	ASSERT_EQUALS(values[1]->getType(), 'I');
+	ASSERT_EQUALS(values[2]->getType(), 'D');
+	ASSERT_EQUALS(values[3]->getType(), 'S');
+	delete values[0];
+	delete values[1];
+	delete values[2];
+	delete values[3];
+	exit(0);
+}
+
+TEST(ValTests, getType) {ASSERT_EXIT_ZERO(getType)}
+
+void setAndGet() {
+	Val* values[4] {new Bool(), new Int(), new Double(), new Str()};
+
+	Bool* b = values[0]->asBool();
+	b->set(false);
+	ASSERT_EQUALS(b->get(), false);
+
+	Int* i = values[1]->asInt();
+	i->set(-12);
+	ASSERT_EQUALS(i->get(), -12);
+
+	Double* d = values[2]->asDouble();
+	d->set(-0.01);
+	ASSERT_TRUE(d->get() - -0.01 < 0.000001);
+
+	Str* s = values[3]->asStr();
+	String v("test");
+	s->set(&v);
+	ASSERT_TRUE(s->get()->equals(&v));
+
+	delete values[0];
+	delete values[1];
+	delete values[2];
+	delete values[3];
+	exit(0);
+}
+
+TEST(ValTests, setAndGet) {ASSERT_EXIT_ZERO(setAndGet)}
