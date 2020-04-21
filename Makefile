67173e731de1ce712f21f4489ba05d7e2e8acb53
danijj89 <danijj89@gmail.com>
2020-04-21 11:02:37 -0400

    moved main file and makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,21 @@
 DOCKER := docker run -ti -v `pwd`:/test w2-gtest:0.1 bash -c
 CXXFLAGS := --std=c++11 -Wall --pedantic -O3 -pthread
 COMPILER := g++
+NETWORK_CONFIGURATIONS := -nodes 2 -socket_backlog 5 -chunk_items 100
+SERVER := -ip 127.0.0.1 -port 9000 -server_ip 127.0.0.1 -server_port 9000
+CLIENT := -ip 127.0.0.1 -port 9001 -server_ip 127.0.0.1 -server_port 9000
+FILE := -file /Users/danijj/Desktop/NEU/sp20/cs4500/final/src/application/wordcount/100k.txt
 
 run: docker_run
 
-local:
-	cd sorer; $(COMPILER) $(CXXFLAGS) main.cpp -o main
+build:
+	$(COMPILER) $(CXXFLAGS) main.cpp -o main
+
+server: build
+	./main $(SERVER) $(NETWORK_CONFIGURATIONS) $(FILE)
+
+client: build
+	./main  $(CLIENT) $(NETWORK_CONFIGURATIONS) $(FILE)
 
 docker_build:
 	$(DOCKER) "cd /test; $(COMPILER) $(CXXFLAGS) main.cpp -o main"
@@ -13,17 +23,14 @@ docker_build:
 docker_run: docker_build
 	$(DOCKER) "cd /test; ./main"
 
+docker_server: docker_build
+	$(DOCKER) "$(SERVER) $(NETWORK_CONFIGURATIONS) $(FILE)"
+
+docker_client: docker_build
+	$(DOCKER) "$(CLIENT) $(NETWORK_CONFIGURATIONS) $(FILE)"
+
 docker_valgrind: docker_build
 	$(DOCKER) "cd /test; valgrind --leak-check=full ./main"
-
-test_setup:
-	$(DOCKER) "cd /test/tests; cmake ."
-
-test: test_setup
-	$(DOCKER) "cd /test/tests; make && ./runTests"
-
-valgrind: test_setup
-	$(DOCKER) "cd /test/tests; valgrind ./runTests"
 
 clean:
 	sudo git clean -df
--- main.cpp
+++ main.cpp
@@ -1,79 +1,13 @@
 
 
-//#include <thread>
-//#include "src/application/demo.h"
-#include <stdio.h>
-#include <signal.h>
-#include "src/util/constants.h"
-#include "src/util/string.h"
-#include "src/network/node.h"
-#include "src/network/network2.h"
+#include "src/network/node_configuration.h"
+#include "src/util/arg_parser.h"
+#include "src/application/wordcount/wordcount.h"
 
-//void thread_job(Demo* app) {
-//	app->run_();
-//}
-
-
-int main() {
-//	int numNodes = 3;
-//	std::thread** workers = new std::thread*[numNodes];
-//	Demo** apps = new Demo*[numNodes];
-//	KVStore** stores = new KVStore*[numNodes];
-//	for (int i = 0; i < numNodes; i++) {
-//		stores[i] = new KVStore(i);
-//		apps[i] = new Demo(i, stores[i]);
-//	}
-//	for (int i = 0; i < numNodes; i++) {
-//		stores[i]->setStores(stores, numNodes);
-//	}
-//	for (int i = 0; i < numNodes; i++) {
-//		workers[i] = new std::thread(thread_job, apps[i]);
-//	}
-//	for (int i = 0; i < numNodes; i++) {
-//		workers[i]->join();
-//	}
-//	delete[] stores;
-//	for (int i = 0; i < numNodes; i++) {
-//		delete apps[i];
-//		delete workers[i];
-//	}
-//	delete[] workers;
-//	delete[] apps;
-	struct sigaction sa;
-	sa.sa_handler = sigchld_handler; // reap all dead processes
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-		perror("sigaction");
-		exit(1);
-	}
-	for (int i = 0; i < NUM_NODES; ++i) {
-		String* ip;
-		int port;
-		switch (i) {
-			case 0:
-				ip = new String(SERVER_IP);
-				port = SERVER_PORT;
-				break;
-			case 1:
-				ip = new String(NODE1_IP);
-				port = PORT1;
-				break;
-			case 2:
-				ip = new String(NODE2_IP);
-				port = PORT2;
-				break;
-			default:
-				assert(false);
-		}
-		if (!fork()) { // Child process
-			Node* node = new Node(i, ip, port);
-			node->initialize();
-			delete node;
-			exit(0);
-		} // main process
-		sleep(3);
-		printf("Started Node %u\n", i);
-	}
+int main(int argc, char** argv) {
+	NodeConfiguration* conf = ArgParser::parse(argc, argv);
+	WordCount app = WordCount(conf);
+	app.run();
+	app.waitShutDown();
 	return 0;
 }
